fb34a3f85407977d8bf959a057ff102557b9c9fc
Ayush Gupta <guptaayush782@outlook.com>
2021-03-05 01:57:57 +0530

    when you upload your old project

---
+++ backup.py
@@ -0,0 +1,32 @@
+import scrapy
+from ..items import QuotetutorialItem
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes'
+    page_number = 2
+    start_urls= [
+        'http://quotes.toscrape.com/page/1/'
+    ]
+
+    def parse(self, response):
+
+        items = QuotetutorialItem()
+
+        all_div_quotes = response.css('div.quote')
+
+        for quotes in all_div_quotes :
+            title = quotes.css('span.text::text').extract()
+            author = quotes.css('.author::text').extract()
+            tag = quotes.css('.tag::text').extract()
+            
+            items['title'] = title
+            items['author'] = author
+            items['tag'] = tag
+            
+            yield items
+        
+        next_page = 'http://quotes.toscrape.com/page/'+ str(QuoteSpider.page_number) +'/'
+
+        if QuoteSpider.page_number < 11:
+            QuoteSpider.page_number +=1
+            yield response.follow(next_page, callback = self.parse)
---
+++ pipelines.py
@@ -0,0 +1,47 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+
+
+
+import mysql.connector
+
+
+class QuotetutorialPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = mysql.connector.connect(
+            host = 'localhost',
+            user = 'root',
+            passwd = 'qwerty123',
+            database = 'myquotes'
+        )
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS quotes_tb""")
+        self.curr.execute("""create table quotes_tb(
+                title text,
+                author text,
+                tag text
+                )""")
+
+    def process_item(self, item, spider):
+        self.store_db(item)
+        return item
+    
+    def store_db(self, item):
+        self.curr.execute("""insert into quotes_tb values (%s,%s,%s)""",(
+            item['title'][0],
+            item['author'][0],
+            item['tag'][0]
+        ))
+        self.conn.commit()
